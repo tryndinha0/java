e07b481f131713781bdb83d65bdabdca21c43e60
daniel <danielsilvagit@gmail.com>
2024-07-23 17:46:54 -0300

    iniciando estudos em java

---
+++ primeiro projeto/src/exercicios/Estudos.java
@@ -0,0 +1,14 @@
+package exercicios;
+
+abstract class Estudos {
+    public static void main(String[] args){
+        System.out.println("Anotação 1");
+        System.out.println("Anotação 2");
+    }
+}
+
+abstract class Soma{
+    public static void main(String[] args) {
+        System.err.println(10+5);
+    }
+}
---
+++ primeiro projeto/src/exercicios/Exercicios.java
@@ -0,0 +1,28 @@
+package exercicios;
+
+// 1 e 2
+class Exercicios{
+    public static void main(String[] args){
+        System.out.println("Olá Daniel");
+        System.out.println("Tudo bem?");
+    }
+}
+//3
+abstract class Estudos {
+    public static void main(String[] args){
+        System.out.println("Anotação 1");
+        System.out.println("Anotação 2");
+    }
+}
+//4
+abstract class Soma{
+    public static void main(String[] args) {
+        System.err.println(10+5);
+    }
+}
+//5
+abstract class Subtracao{
+    public static void main(String[] args) {
+        System.out.println(10-5);
+    }
+}
